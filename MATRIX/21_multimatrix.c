c5580ad3012aa4570fc9a6323bf483a63510a2b3
Anubhav Shukla <anubhav77175@gmail.com>
2025-09-28 18:48:15 +0530

    SUM OF MATRIX

---
+++ MATRIX/21_multimatrix.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+    int a[2][2]={{2,3},{4,1}};
+    int b[2][2]={{1,5},{3,2}};
+    int c[2][2];
+
+    for (int i = 0; i < 2; i++)
+    {
+        for (int j = 0; j < 2; j++)
+        {
+            c[i][j]=a[i][j]*b[i][j];
+        }
+        
+    }
+    for (int i = 0; i < 2; i++)
+    {
+        printf("\n");
+        for (int j = 0; j < 2; j++)
+        {
+            printf("%d ",c[i][j]);
+        }
+        
+    }
+    return 0;
+}
